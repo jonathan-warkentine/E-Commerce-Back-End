8859e5e27b820015789cfec24dfffcb1099dda47
Jonathan Warkentine <jonathan.warkentine@gmail.com>
2022-04-07 16:13:11 -0400

    mapping and CRUD operations functional

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,19 +4,58 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      include: [Category, Tag]
+    });
+    if (!productData){
+      res.status(404).json({error: 'no data available'});
+    }
+    else {
+      res.status(200).json(productData);
+    }
+  }
+  catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [Category, Tag]
+    });
+
+    if (!productData){
+      res.status(404).json({error: 'no such product'});
+    }
+    else {
+      res.status(200).json(productData);
+    }
+  }
+  catch (error){
+    res.status(500).json(error);
+  }
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try{
+    if (!req.body){
+      res.status(400).json({error: 'no body provided'});
+    }
+    else {
+      res.status(200).json(await Product.create(req.body));
+    }
+  }
+  catch (error) {
+    res.status(500).json(error);
+  }
+
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -25,26 +64,26 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
-    .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
-      }
-      // if no product tags, just respond
-      res.status(200).json(product);
-    })
-    .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+//   Product.create(req.body)
+//     .then((product) => {
+//       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+//       if (req.body.tagIds.length) {
+//         const productTagIdArr = req.body.tagIds.map((tag_id) => {
+//           return {
+//             product_id: product.id,
+//             tag_id,
+//           };
+//         });
+//         return ProductTag.bulkCreate(productTagIdArr);
+//       }
+//       // if no product tags, just respond
+//       res.status(200).json(product);
+//     })
+//     .then((productTagIds) => res.status(200).json(productTagIds))
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(400).json(err);
+//     });
 });
 
 // update product
@@ -89,8 +128,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    if (!productData){
+      res.status(404).json({error: 'no such product'});
+    }
+    else {
+      res.status(200).json(productData);
+    }
+  }
+  catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,102 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+  try {
+    const tagData = await Tag.findAll(
+      {
+        include: Product
+      }
+    );
+  
+    res.status(200).json(tagData);
+  } catch (error){
+    res.status(500).json(error);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: Product
+    })
+  
+    if (!tagData){
+      res.status(404).json({error: "no such tag"})
+    }
+    else{
+      res.status(200).json(tagData);
+    }
+  }
+  catch (error){
+    res.status(500).json(error);
+  }
+  
+  
+
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+router.post('/', async (req, res) => {
+  try {
+    if (req.body){
+      const newTag = await Tag.create(req.body);
+      res.status(200).json(newTag)
+    }
+    else {
+      res.status(400).json({error: 'no tag data provided'});
+    }
+  }
+  catch (error){
+    res.status(500).json(error);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    if (!req.body){
+      res.status(400).json({error: "no update data provided"});
+    }
+    else {
+      const tagData = await Tag.update(req.body, {
+        where: {
+          id: req.params.id
+        }
+      });
+      if (!tagData){
+        res.status(404).json({error: 'no such tag'})
+      } 
+      else {
+        res.status(200).json(tagData);
+      }
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    if (!req.params.id){
+      res.status(400).json({error: 'no id provided'});
+    }
+    else {
+      const tagData = await Tag.destroy({
+        where: {
+          id: req.params.id
+        }
+      });
+      if (!tagData) {
+        res.status(404).json({error: 'no such tag'})
+      }
+      else {
+        res.status(200).json(tagData);
+      }
+    }
+  } catch (error){
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
